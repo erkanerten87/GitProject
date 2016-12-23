4647b2e61c913d717a449bf7be4cfa74234f840a
eerten <eerten@ziraatteknoloji.com>
2016-12-23 14:58:15 +0300

    BranchMasterUpdated

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import "gopkg.in/redis.v5"
 
 func main() {
 	fmt.Printf("Hello world")
+	ExampleNewClient()
+}
+
+func ExampleNewClient() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
 }
